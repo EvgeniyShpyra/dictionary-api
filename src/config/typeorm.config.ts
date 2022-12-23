4f719d6710cacefdfca60cfd5b005c7e09b8f9aa
Sergey <forsubscriptions2019@gmail.com>
2022-12-23 21:15:26 +0200

    Added config

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+export function getTypeOrmConfig(
+  configService: ConfigService,
+): TypeOrmModuleOptions {
+  return {
+    type: 'postgres',
+    host: configService.get('DB_HOST') || 'localhost',
+    port: +configService.get<number>('DB_PORT') || 5432,
+    username: configService.get('DB_USERNAME') || 'postgres',
+    password: configService.get('DB_PASSWORD') || 'password',
+    // database: configService.get('DB_NAME') || 'postgres',
+
+    autoLoadEntities: true,
+    synchronize: true,
+  };
+}
