02c7a7a19773c8af640eb979281cb25ee7ec16dc
Sergey <forsubscriptions2019@gmail.com>
2023-01-02 18:53:20 +0200

    Added password validation

--- src/auth/dto/signup-user.dto.ts
+++ src/auth/dto/signup-user.dto.ts
@@ -1,10 +1,13 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, Matches } from 'class-validator';
 
 export class SignupUserDto {
   @IsString()
   username: string;
   @IsEmail()
   email: string;
+  @Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm, {
+    message: 'Password is too weak',
+  })
   @IsString()
   password: string;
 }
