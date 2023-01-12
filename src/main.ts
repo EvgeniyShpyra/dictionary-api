5bef2f3377888c7524e79dab354f343b4d0b4098
Sergey <forsubscriptions2019@gmail.com>
2023-01-12 07:36:22 +0200

    Updated CORS

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('/api');
   app.enableCors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://dictionary-ashy.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
   app.useGlobalPipes(
