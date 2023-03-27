431624c7f1b15d62b70760b1c7794b37f39e1e75
Sergey <spushkovskiy@gmail.com>
2023-03-27 21:34:42 +0300

    Added Access-Control-Allow middleware

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { responseMiddleware } from './middleware/response.middleware';
 
 async function bootstrap() {
   const logger = new Logger(AppModule.name);
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.use(responseMiddleware);
   app.enableCors({
     credentials: true,
     origin: ['http://localhost:3000', 'https://dictionary-ashy.vercel.app'],
